14f7b1fbbe6566c83978c1168697d3fcbbc85f77
Sang Hoo Park <sksk8922@gmail.com>
2024-09-07 20:53:37 +0900

    refactor: 배열 비교 테스트 기능별로 분리

--- test/jest-practice.spec.ts
+++ test/jest-practice.spec.ts
@@ -32,31 +32,46 @@ describe('objectContaining/ toMatchObject Test', () => {
     }); // SUCCESS
   });
 
-  it('array objectContaining Test', () => {
-    // GOAL : 배열의 객체가 일부만 일치하는지 확인
+  describe('arrayContaining 과 toMatchObject 기능 비교 테스트', () => {
+    it('array objectContaining Test', () => {
+      // GOAL : 배열의 객체가 일부만 일치하는지 확인
 
-    // Given
-    const products = [
-      { id: 1, name: 'Laptop', price: 1000 },
-      { id: 2, name: 'Mouse', price: 50 },
-      { id: 3, name: 'Keyboard', price: 80 },
-    ];
+      // Given
+      const products = [
+        { id: 1, name: 'Laptop', price: 1000 },
+        { id: 2, name: 'Mouse', price: 50 },
+        { id: 3, name: 'Keyboard', price: 80 },
+      ];
 
-    // 배열에서 'Laptop'이 포함된 객체를 확인
-    expect(products).toEqual(expect.arrayContaining([{ id: 1, name: 'Laptop', price: 1000 }])); // SUCCESS
+      // 배열에서 'Laptop'이 포함된 객체를 확인
+      expect(products).toEqual(expect.arrayContaining([{ id: 1, name: 'Laptop', price: 1000 }])); // SUCCESS
 
-    // 배열의 전체가 아닌 특정 객체에 대한 부분 일치를 확인
-    expect(products).toEqual(expect.arrayContaining([expect.objectContaining({ name: 'Laptop' })])); // SUCCESS
+      // 배열의 전체가 아닌 특정 객체에 대한 부분 일치를 확인
+      expect(products).toEqual(expect.arrayContaining([expect.objectContaining({ name: 'Laptop' })])); // SUCCESS
 
-    // toMatchObject 를 사용한 테스트
-    expect(products).toMatchObject([{ name: 'Laptop' }]); // FAIL
-    expect(products).toMatchObject([{ id: 1 }, { id: 2 }, { id: 3 }]); // SUCCESS
-    expect(products).toMatchObject([{ id: 1 }, { id: 3 }, { id: 2 }]); // FAIL
+      // 결론 expect.arrayContaining 은 배열의 일부만 일치해도 성공한다.
+    });
 
-    // 만약 나였다면 ... ?
-    const result = products.find((product) => product.name === 'Laptop');
-    expect(result).toMatchObject({ name: 'Laptop' }); // SUCCESS
+    it('toMatchObject Test ', () => {
+      // GOAL : 배열의 객체가 일부만 일치하는지 확인
 
-    // 결론 toMatchObject 는 배열의 전체가 일치해야 성공하는 반면, expect.arrayContaining 은 배열의 일부만 일치해도 성공한다.
+      // Given
+      const products = [
+        { id: 1, name: 'Laptop', price: 1000 },
+        { id: 2, name: 'Mouse', price: 50 },
+        { id: 3, name: 'Keyboard', price: 80 },
+      ];
+
+      // toMatchObject 를 사용한 테스트
+      expect(products).not.toMatchObject([{ name: 'Laptop' }]); // FAIL
+      expect(products).toMatchObject([{ id: 1 }, { id: 2 }, { id: 3 }]); // SUCCESS
+      expect(products).not.toMatchObject([{ id: 1 }, { id: 3 }, { id: 2 }]); // FAIL
+
+      // 만약 나였다면 ... ?
+      const result = products.find((product) => product.name === 'Laptop');
+      expect(result).toMatchObject({ name: 'Laptop' }); // SUCCESS
+
+      // 결론 toMatchObject 는 배열을 비교할 때 길이와 순서가 같아야 성공한다.
+    });
   });
 });
